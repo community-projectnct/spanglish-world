9d5fbab879b7f94c2718bed7867233def4762378
Community Project <communityprojectnct@gmail.com>
2024-10-14 20:09:00 +0530

    ui design

--- app/(tabs)/her.tsx
+++ app/(tabs)/her.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function HERScreen() {
+  const injectedJavaScript = `
+    (function() {
+      // Hide the specific navigation element
+      var navElement = document.querySelector('.elementor-element.elementor-widget-ekit-nav-menu');
+      if (navElement) {
+        navElement.style.display = 'none';
+      }
+    })();
+    true; // Required for the injected JavaScript to execute properly
+  `;
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>H.E.R. Screen</Text>
+    <View style={styles.container}>
+      <WebView 
+        source={{ uri: 'https://spanglishworld.ca/mujeres-woman-women-mujeres/' }} // Replace with your desired URL
+        style={{ flex: 1 }} 
+        injectedJavaScript={injectedJavaScript} // Inject the JavaScript
+        javaScriptEnabled={true} // Ensure JavaScript is enabled
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
--- app/(tabs)/laPortada.tsx
+++ app/(tabs)/laPortada.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
+import { View, Text ,StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
 export default function LaPortadaScreen() {
+  const injectedJavaScript = `
+    (function() {
+      // Hide the specific navigation element
+      var navElement = document.querySelector('.elementor-element.elementor-widget-ekit-nav-menu');
+      if (navElement) {
+        navElement.style.display = 'none';
+      }
+    })();
+    true; // Required for the injected JavaScript to execute properly
+  `;
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>La Portada Screen</Text>
-    </View>
+    <View style={styles.container}>
+    <WebView 
+      source={{ uri: 'https://www.laportadacanada.com/' }} // Replace with your desired URL
+      style={{ flex: 1 }} 
+      injectedJavaScript={injectedJavaScript} // Inject the JavaScript
+      javaScriptEnabled={true} // Ensure JavaScript is enabled
+    />
+  </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
